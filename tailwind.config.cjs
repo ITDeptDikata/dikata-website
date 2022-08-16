a57f5bdcd2b2e86143c46d9dec028008bc1d762f
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-08-16 22:26:51 +0700

    Setup fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                heading: ["Hind Madurai", ...defaultTheme.fontFamily.sans],
+                sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
     plugins: [],
 };
