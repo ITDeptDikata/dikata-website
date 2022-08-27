e19da1f326904a4c92df7c9f425b94e695186fcf
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-08-27 10:40:36 +0700

    Create text classes

--- src/components/Layouts/TextSection.jsx
+++ src/components/Layouts/TextSection.jsx
@@ -2,11 +2,9 @@
  * Use this for a section with heading and text (e.g. Tentang Kami, Visi Kami, dst.)
  */
 const TextSection = ({ children, title }) => (
-    <section className="h-max w-max">
-        <h2 className="text-shadow-sm font-heading text-2xl font-bold">{title}</h2>
-        <p className="font-heading text-[0.9375rem] font-normal leading-5">
-            {children}
-        </p>
+    <section className="h-max w-full">
+        <h2 className="text-h2">{title}</h2>
+        <p className="text-body">{children}</p>
     </section>
 );
 
