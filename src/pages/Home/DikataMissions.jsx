1adb7fbcfb6e635c8306f4ebc417da20b54b4ab0
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-08-27 10:40:43 +0700

    Create `DikataMissions`

---
+++ src/pages/Home/DikataMissions.jsx
@@ -0,0 +1,33 @@
+const ListItem = ({ children }) => (
+    <li className="list-yellow-checkbox flex gap-3">
+        <img
+            src="images/list-checkbox.png"
+            className="h-[25px] w-[25px]"
+        />
+        {children}
+    </li>
+);
+
+const DikataMissions = () => {
+    return (
+        <section>
+            <h2 className="text-h2 mb-[0.4375rem]">Misi Kami</h2>
+            <ul className="text-body space-y-4">
+                <ListItem>
+                    Memperluas dan mempermudah akses sobat literasi dalam berkarya
+                    dan berkontribusi.
+                </ListItem>
+                <ListItem>
+                    Menjaga relasi yang harmonis antar pihak internal dan eksternal
+                    Dikata.
+                </ListItem>
+                <ListItem>
+                    Melaksanakan berbagai macam program secara konsisten, aktif, dan
+                    sustainable.
+                </ListItem>
+            </ul>
+        </section>
+    );
+};
+
+export default DikataMissions;
