a699acad2611c084a6b3444d5e0bcf8b490403e7
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-08-28 09:12:18 +0700

    Optimize `Icons`

--- src/components/Icons/DikataIconCircle.jsx
+++ src/components/Icons/DikataIconCircle.jsx
@@ -3,23 +3,21 @@
  * @param {{noText?: boolean} & import("react").AllHTMLAttributes} props
  * @returns
  */
-const DikataIconCircle = ({ noText = false, ...props }) => {
-    return (
-        <div
-            className="flex w-full flex-row gap-x-1 align-middle"
-            {...props}
-        >
-            <img
-                alt="dikata logo"
-                title="dikata logo"
-                className="w- object-cover"
-                src="/images/dikata-logo-circle.png"
-            />
-            {!noText && (
-                <p className="font-inter text-lg font-bold text-black">dikata</p>
-            )}
-        </div>
-    );
-};
+const DikataIconCircle = ({ noText = false, ...props }) => (
+    <div
+        className="flex w-full flex-row gap-x-1 align-middle"
+        {...props}
+    >
+        <img
+            alt="dikata logo"
+            title="dikata logo"
+            className="object-cover"
+            src="/images/dikata-logo-circle.png"
+        />
+        {!noText && (
+            <p className="font-inter text-lg font-bold text-black">dikata</p>
+        )}
+    </div>
+);
 
 export default DikataIconCircle;
