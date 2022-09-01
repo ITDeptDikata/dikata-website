e465d72d84c9442483e876048b91372b8ae13841
Damar Buana Murti <damarbm30@gmail.com>
2022-09-01 21:13:21 +0700

    Update Mobile Footer template

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import FooterAddress from "./FooterAddress";
+import FooterIcon from "./FooterIcon";
 import FooterImage from "./FooterImage";
+import FooterLink from "./FooterLink";
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className="bg-[#383839]">
             <FooterImage />
+            <FooterIcon />
+            <FooterLink />
+            <FooterAddress />
         </footer>
     );
 };
---
+++ src/components/Footer/FooterAddress.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const FooterAddress = () => {
+    return (
+        <div className="ml-[1.8375rem]">
+            <p className="mt-[4.25rem]  mb-5 w-[45%] font-sans text-xs text-white-50">
+                <span className="text-brand">Official Reading House of Dikata</span>{" "}
+                - Jl. Teuku Cik Ditiro, No. 17, Rt. 07/01, Pesantunan, Wanasari,
+                Brebes, Jawa Tengah, Indonesia 52221
+            </p>
+            <hr className="mr-[1.8375rem] mb-5 border-2 border-brand" />
+            <figure className="flex items-center pb-[1.92rem]">
+                <img
+                    src="images/dikata-logo-circle.png"
+                    alt="dikata-logo"
+                />
+                <figcaption className="ml-[0.3175rem] font-inter font-bold text-white">
+                    dikata
+                </figcaption>
+            </figure>
+        </div>
+    );
+};
+
+export default FooterAddress;
