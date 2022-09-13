92b8b9f9a75f8b69c3fbde03132d0ce1e5161125
Damar Buana Murti <damarbm30@gmail.com>
2022-09-13 19:22:08 +0700

    Update Teams section

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Teams from "./components/Teams";
+
+const App = () => {
+    return (
+        <>
+            <Teams />
+        </>
+    );
+};
+
+export default App;
