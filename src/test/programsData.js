2204b2ec977ae28e8ffddb7bb4c1701a376a1963
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-09-15 18:32:59 +0700

    Add missing `programsData`

---
+++ src/test/programsData.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import("../utils/dikata-server/dikata-event").DikataEvent[]}
+ */
+export const programsData = [
+    {
+        description: `Rumah Baca Dikata merupakan the big project yang diinisiasi oleh Dikata. Dirintis pada tanggal 8 November 2020.
+
+        Sebagai bagian dari implementasi program penyaluran edukasi berbasis peningkatan soft skill dan kreativitas untuk masyarakat sekitar.`,
+        title: "Fake Program 1",
+        image: "https://picsum.photos/100/100",
+        id: "fjeijfieojfiojj",
+    },
+    {
+        description: `Rumah Baca Dikata merupakan the big project yang diinisiasi oleh Dikata. Dirintis pada tanggal 8 November 2020.
+
+        Sebagai bagian dari implementasi program penyaluran edukasi berbasis peningkatan soft skill dan kreativitas untuk masyarakat sekitar.`,
+        title: "Fake Program 2",
+        image: "https://picsum.photos/150/300",
+        id: "fjeijfieojfiojj",
+    },
+    {
+        description: `Rumah Baca Dikata merupakan the big project yang diinisiasi oleh Dikata. Dirintis pada tanggal 8 November 2020.
+
+        Sebagai bagian dari implementasi program penyaluran edukasi berbasis peningkatan soft skill dan kreativitas untuk masyarakat sekitar.`,
+        title: "Fake Program 3",
+        image: "https://picsum.photos/300/300",
+        id: "fjeijfieojfiojj",
+    },
+];
