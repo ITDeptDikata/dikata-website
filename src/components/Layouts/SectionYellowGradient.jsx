1cfbbc8592b9a359a7937acb6b89570e74c78e1d
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-09-15 21:12:47 +0700

    Add id prop to `SectionYellowGradient`

--- src/components/Layouts/SectionYellowGradient.jsx
+++ src/components/Layouts/SectionYellowGradient.jsx
@@ -3,12 +3,19 @@
  * @param {{children: import("react").ReactNode, dir: "top" | "bottom"}} param0
  * @returns
  */
-const SectionYellowGradient = ({ children, dir = "top" }) => {
+const SectionYellowGradient = ({ children, id, dir = "top" }) => {
     const className = `${
         dir === "top" ? "bg-gradient-to-t" : "bg-gradient-to-b"
     } from-yellow-to-white relative`;
 
-    return <section className={className}>{children}</section>;
+    return (
+        <section
+            className={className}
+            id={id}
+        >
+            {children}
+        </section>
+    );
 };
 
 export default SectionYellowGradient;
