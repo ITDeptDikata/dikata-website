a2c3f001e9146509bd782d20555edc81e86c39d4
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-09-20 00:32:35 +0700

    Create `DikataMagazine`

---
+++ src/pages/Home/DikataMagazine.jsx
@@ -0,0 +1,26 @@
+const DikatMagazine = () => {
+    return (
+        <section className="flex flex-col space-y-5 bg-brand-50 px-[1.3125em]">
+            <h2 className="text-h2">Dikata Magazine</h2>
+            <img
+                src="/images/dikata_magazine_cover.png"
+                alt="dikata magazine cover"
+                className="mx-auto w-72 rounded-lg drop-shadow-xl"
+            />
+            <p className="text-body">
+                Dikata menyapa anda dengan majalah perdana kami! Jika manta tertarik
+                mengetahui serba serbi info dari dikata, yuk download saja majalah
+                kami melalui tombol dibawah ini!
+            </p>
+            <a
+                className="rounded-md bg-black p-3 text-center text-brand-50 shadow-md transition-colors hover:bg-white-50 active:bg-gray-200"
+                download="Dikata Menyapa"
+                href="dikata-magazine.pdf"
+            >
+                Download Dikata Menyapa
+            </a>
+        </section>
+    );
+};
+
+export default DikatMagazine;
